9729b97bde442aea824780cf76c8fa3d5fea8a91
Klaas Schoute <klaas_schoute@hotmail.com>
2023-03-29 00:29:33 +0200

    Change freeze_time from UTC to CET

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,9 +10,9 @@ from energyzero import Electricity, EnergyZero, EnergyZeroNoDataError, Gas
 from . import load_fixtures
 
 
-@pytest.mark.freeze_time("2022-12-07 14:00:00 UTC")
+@pytest.mark.freeze_time("2022-12-07 15:00:00 CET")
 async def test_electricity_model(aresponses: ResponsesMockServer) -> None:
-    """Test the electricity model at 14:00:00 UTC."""
+    """Test the electricity model at 15:00:00 CET."""
     aresponses.add(
         "api.energyzero.nl",
         "/v1/energyprices",
@@ -121,9 +121,9 @@ async def test_no_electricity_data(aresponses: ResponsesMockServer) -> None:
             await client.energy_prices(start_date=today, end_date=today)
 
 
-@pytest.mark.freeze_time("2022-12-07 14:00:00 UTC")
+@pytest.mark.freeze_time("2022-12-07 15:00:00 CET")
 async def test_gas_model(aresponses: ResponsesMockServer) -> None:
-    """Test the gas model at 14:00:00 UTC."""
+    """Test the gas model at 15:00:00 CET."""
     aresponses.add(
         "api.energyzero.nl",
         "/v1/energyprices",
@@ -149,9 +149,9 @@ async def test_gas_model(aresponses: ResponsesMockServer) -> None:
         assert gas.price_at_time(next_hour) == 1.47
 
 
-@pytest.mark.freeze_time("2022-12-07 03:00:00 UTC")
+@pytest.mark.freeze_time("2022-12-07 04:00:00 CET")
 async def test_gas_morning_model(aresponses: ResponsesMockServer) -> None:
-    """Test the gas model in the morning at 03:00:00 UTC."""
+    """Test the gas model in the morning at 04:00:00 CET."""
     aresponses.add(
         "api.energyzero.nl",
         "/v1/energyprices",
