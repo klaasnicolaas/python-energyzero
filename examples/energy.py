a9a767fb89c275a7f0192f5194e47a9c5fa6b8c6
Bas Tossings <12195470+BasTossings@users.noreply.github.com>
2025-06-01 15:01:27 +0000

    Updated samples

--- examples/energy.py
+++ examples/energy.py
@@ -23,26 +23,26 @@ def _print_energy_price_info(energy_prices: EnergyPrices) -> None:
         return
 
     today_extreme_prices = energy_prices.extreme_prices
+    next_hour = energy_prices.utcnow() + timedelta(hours=1)
+    current_price = energy_prices.current_price
+    next_hour_price = energy_prices.price_at_time(next_hour)
+    today_highest_price_time_range = energy_prices.highest_price_time_range
+    today_lowest_price_time_range = energy_prices.lowest_price_time_range
 
     if today_extreme_prices is not None:
         print(f"Max price: {_price_to_string(today_extreme_prices[1])}")
         print(f"Min price: {_price_to_string(today_extreme_prices[0])}")
 
     print(f"Average price: {_price_to_string(energy_prices.average_price)}")
-    print(f"Percentage: {energy_prices.pct_of_max_price}%")
 
-    today_highest_price_time_range = energy_prices.highest_price_time_range
-    today_lowest_price_time_range = energy_prices.lowest_price_time_range
+    if current_price is not None:
+        print(f"Percentage: {energy_prices.pct_of_max_price}%")
 
     if today_highest_price_time_range is not None:
         print(f"High time: {today_highest_price_time_range.astimezone(local_tz)}")
 
     if today_lowest_price_time_range is not None:
         print(f"Lowest time: {today_lowest_price_time_range.astimezone(local_tz)}")
-
-    next_hour = energy_prices.utcnow() + timedelta(hours=1)
-    current_price = energy_prices.current_price
-    next_hour_price = energy_prices.price_at_time(next_hour)
 
     if current_price is not None:
         print(f"Current hourprice: {_price_to_string(current_price)}")
@@ -51,12 +51,15 @@ def _print_energy_price_info(energy_prices: EnergyPrices) -> None:
         print(f"Next hourprice: {_price_to_string(next_hour_price)}")
 
     best_hours = energy_prices.hours_priced_equal_or_lower
-    print(f"Hours lower or equal than current price: {best_hours}")
+
+    if best_hours is not None and best_hours > 0:
+        print(f"Hours lower or equal than current price: {best_hours}")
 
 
 async def main() -> None:
     """Show example on fetching the energy prices from EnergyZero."""
     async with EnergyZero() as client:
+        # Note: The devcontainer's timezone is always UTC
         # Simulate a call from a client running in the Netherlands
         environ["TZ"] = "Europe/Amsterdam"
         tzset()
--- examples/prices_list.py
+++ examples/prices_list.py
@@ -1,24 +1,67 @@
 """Asynchronous Python client for the EnergyZero API."""
 
 import asyncio
-from datetime import date
+from datetime import UTC, datetime, tzinfo
+from os import environ
+from time import tzset
 
-from energyzero import EnergyZero, VatOption
+from energyzero import EnergyZero, TimeRange, VatOption
+
+
+def _price_to_string(price: float | None) -> str:
+    if price is None:
+        return "Unknown"
+
+    return f"€{price:0.2f} (€{price})"
+
+
+def _range_and_price_to_str(time_range: TimeRange, price: float, tz: tzinfo) -> str:
+    return f"{time_range.astimezone(tz)}: {_price_to_string(price)}"
+
+
+def _price_list_item_to_str(entry: dict[str, float | TimeRange], tz: tzinfo) -> str:
+    time_range = entry["timerange"]
+    price = entry["price"]
+
+    if (not isinstance(time_range, TimeRange)) or (not isinstance(price, float)):
+        msg = "Dictionary does not contain a valid timerange-price combination"
+        raise TypeError(msg)
+
+    return _range_and_price_to_str(time_range, price, tz)
 
 
 async def main() -> None:
     """Show example on fetching the timestamp lists from EnergyZero."""
     async with EnergyZero(vat=VatOption.INCLUDE) as client:
-        today = date(2023, 12, 5)
-        energy = await client.energy_prices(start_date=today, end_date=today)
-        gas = await client.gas_prices(start_date=today, end_date=today)
+        # Note: The devcontainer's timezone is always UTC
+        # Simulate a call from a client running in the Netherlands
+        environ["TZ"] = "Europe/Amsterdam"
+        tzset()
 
-        print("--- ENERGY ---")
-        print(energy.timestamp_prices)
+        local_tz = datetime.now(UTC).astimezone().tzinfo
+        today = datetime.now(tz=local_tz).date()
+
+        if local_tz is None:
+            print("Error: timezone could not be determined")
+            return
+
+        energy = await client.electricity_prices_ex(start_date=today, end_date=today)
+        gas = await client.gas_prices_ex(start_date=today, end_date=today)
+
+        electricity_prices_str = (
+            _price_list_item_to_str(entry, local_tz)
+            for entry in energy.timestamp_prices
+        )
+        gas_prices_str = (
+            _price_list_item_to_str(entry, local_tz) for entry in gas.timestamp_prices
+        )
+
+        print("--- ELECTRICITY ---")
+        print("\n".join(electricity_prices_str))
         print()
 
         print("--- GAS ---")
-        print(gas.timestamp_prices)
+        print("\n".join(gas_prices_str))
 
 
 if __name__ == "__main__":
