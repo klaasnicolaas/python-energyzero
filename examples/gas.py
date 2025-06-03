08fe025fe43da5c238e092fed7c154fd7c4d4e8c
Klaas Schoute <klaas_schoute@hotmail.com>
2025-06-03 14:01:23 +0200

    Use helper to_datetime_string for gas and energy prices (#783)
    * Use helper `to_datetime_string` for gas and energy prices
    
    * Update example scripts to use correct dates and format prices in euros
    
    * Clarify docstring for `to_datetime_string` to specify local timezone for base date

--- examples/gas.py
+++ examples/gas.py
@@ -9,18 +9,18 @@ from energyzero import EnergyZero, VatOption
 async def main() -> None:
     """Show example on fetching the gas prices from EnergyZero."""
     async with EnergyZero(vat=VatOption.INCLUDE) as client:
-        today = date(2023, 12, 5)
+        today = date(2025, 6, 3)
 
         gas_today = await client.gas_prices(start_date=today, end_date=today)
         print()
         print("--- GAS TODAY ---")
-        print(f"Max price: {gas_today.extreme_prices[1]}")
-        print(f"Min price: {gas_today.extreme_prices[0]}")
-        print(f"Average price: {gas_today.average_price}")
+        print(f"Max price: €{gas_today.extreme_prices[1]}")
+        print(f"Min price: €{gas_today.extreme_prices[0]}")
+        print(f"Average price: €{gas_today.average_price}")
         print()
-        print(f"Current price: {gas_today.current_price}")
+        print(f"Current price: €{gas_today.current_price}")
         next_hour = gas_today.utcnow() + timedelta(hours=1)
-        print(f"Next price: {gas_today.price_at_time(next_hour)}")
+        print(f"Next price: €{gas_today.price_at_time(next_hour)}")
 
 
 if __name__ == "__main__":
